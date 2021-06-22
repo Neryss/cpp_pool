1458a122b8b8ee154b25e5ccc81ad3649995a72d
neryss <neryssistos@gmail.com>
2021-06-22 11:25:26 +0200

    mieux

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -1,13 +1,18 @@
 #include <iostream>
 #include <string>
 
-int	main(void)
+int	main(int argc, char **argv)
 {
-	std::string	str;
-
-	std::getline(std::cin, str);
-	for (int i = 0; i < (int)str.length(); i++)
-		putchar(toupper(str[i]));
+	if (argc == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return (0);
+	}
+	for (int i = 1; i < argc; i++)
+	{
+		for (int j = 0; j < (int)strlen(argv[i]); j++)
+			putchar(toupper(argv[i][j]));
+	}
 	putchar('\n');
 	return (0);
 }
