7ab952f32f7119f9524d2ce5ed58ab5dd6ce280b
neryss <neryssistos@gmail.com>
2021-06-22 16:52:07 +0200

    added some "proof of concept"

--- day01/ex00/main.cpp
+++ day01/ex00/main.cpp
@@ -3,7 +3,10 @@
 int	main(void)
 {
 	ponyOnTheHeap(true, true, "Buttstalion");
-	ponyOnTheStack(false, false, "Bobert");
+	ponyOnTheHeap(false, false, "");
+	ponyOnTheHeap(false, true, "John");
+	ponyOnTheStack(true, true, "Buttstalion");
 	ponyOnTheStack(false, false, "");
+	ponyOnTheStack(false, true, "John");
 	return (0);
 }
