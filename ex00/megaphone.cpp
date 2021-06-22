83b85d1ff91abc490ee7b78311b44b31839ae4a3
neryss <neryssistos@gmail.com>
2021-06-22 11:12:49 +0200

    Plus dur que ce que je pensais

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -1,8 +1,13 @@
-#include <cstring>
-#include <cstdio>
 #include <iostream>
+#include <string>
 
-int	main(int argc, char **argv)
+int	main(void)
 {
-	
+	std::string	str;
+
+	std::getline(std::cin, str);
+	for (int i = 0; i < (int)str.length(); i++)
+		putchar(toupper(str[i]));
+	putchar('\n');
+	return (0);
 }
