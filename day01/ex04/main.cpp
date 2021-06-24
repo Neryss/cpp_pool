0a24735a446fdfb841d0cb7a25341e75b76b510a
neryss <neryssistos@gmail.com>
2021-06-24 16:41:45 +0200

    it's kinda working

--- day01/ex04/main.cpp
+++ day01/ex04/main.cpp
@@ -1,3 +1,40 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+
+int	ft_error(std::string error, int ret)
+{
+	std::cout << error << std::endl;
+	return (ret);
+}
+
+int	main(int argc, char **argv)
+{
+	std::ifstream	file;
+	std::string		line;
+	std::string		before;
+	std::string		after;
+	std::string		name;
+	size_t			found;
+
+	if (argc != 4)
+		return (ft_error("Wrong number of arguments", 1));
+	file.open(argv[1]);
+	name = argv[1];
+	name.append(".replace");
+	std:: ofstream replace (name);
+	before = argv[2];
+	after = argv[3];
+	if (!file.is_open())
+		return (ft_error("Error during file opening", 1));
+	while (getline(file, line))
+	{
+		found = line.find(before);
+		if (found != line.npos)
+		{
+			std::cout << "Found one!" << std::endl;
+		}
+		replace << line << std::endl;
+	}
+	file.close();
+}
