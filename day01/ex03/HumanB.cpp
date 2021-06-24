628eab2aa9e5bbd5dd69b4774826908a54bcf3c0
neryss <neryssistos@gmail.com>
2021-06-24 15:49:09 +0200

    Ok c'est bon

--- day01/ex03/HumanB.cpp
+++ day01/ex03/HumanB.cpp
@@ -2,12 +2,12 @@
 
 void	HumanB::setWeapon(Weapon &n_weapon)
 {
-	weapon = n_weapon;
+	weapon = &n_weapon;
 }
 
 void	HumanB::attack()
 {
-	std::cout << name << " attacks with his " << weapon.getType() << std::endl;
+	std::cout << name << " attacks with his " << weapon->getType() << std::endl;
 }
 
 HumanB::~HumanB()
