1f808af82fc84850bbbacaafd156c4225f2a6cdb
neryss <neryssistos@gmail.com>
2021-06-24 17:44:01 +0200

    added clean, wip karen

---
+++ day01/ex05/Karen.cpp
@@ -0,0 +1,26 @@
+#include "Karen.hpp"
+
+void	debug(void)
+{
+	std::cout << "I love to get extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I just love it!" << std::endl;
+}
+
+void	info(void)
+{
+	std::cout << " cannot believe adding extrabacon cost more money. You don’t put enough! If you did I would not have to askfor it!" << std::endl;
+}
+
+void	warning(void)
+{
+	std::cout << " cannot believe adding extrabacon cost more money. You don’t put enough! If you did I would not have to askfor it!" << std::endl;
+}
+
+void	error(void)
+{
+	std::cout << "This is unacceptable, I want to speak to the manager now." << std::endl;
+}
+
+void	Karen::complain(std::string level)
+{
+	
+}
