a5ab82746e05f3f7a50e1829ef5f5a095f591338
neryss <neryssistos@gmail.com>
2021-06-24 16:47:05 +0200

    cleaned up a bit

--- day01/ex04/main.cpp
+++ day01/ex04/main.cpp
@@ -8,14 +8,29 @@ int	ft_error(std::string error, int ret)
 	return (ret);
 }
 
+void	replace_inline(std::ifstream &file, std::ofstream &replace, std::string &before, std::string &after)
+{
+	std::string	line;
+	size_t		found;
+	(void)after;
+
+	while (getline(file, line))
+	{
+		found = line.find(before);
+		if (found != line.npos)
+		{
+			std::cout << "Found one!" << std::endl;
+		}
+		replace << line << std::endl;
+	}
+}
+
 int	main(int argc, char **argv)
 {
 	std::ifstream	file;
-	std::string		line;
 	std::string		before;
 	std::string		after;
 	std::string		name;
-	size_t			found;
 
 	if (argc != 4)
 		return (ft_error("Wrong number of arguments", 1));
@@ -27,14 +42,6 @@ int	main(int argc, char **argv)
 	after = argv[3];
 	if (!file.is_open())
 		return (ft_error("Error during file opening", 1));
-	while (getline(file, line))
-	{
-		found = line.find(before);
-		if (found != line.npos)
-		{
-			std::cout << "Found one!" << std::endl;
-		}
-		replace << line << std::endl;
-	}
+	replace_inline(file, replace, before, after);
 	file.close();
 }
