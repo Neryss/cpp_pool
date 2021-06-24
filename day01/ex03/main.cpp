1cd2ef2c95d381c45adde058e9e8083d1071a722
neryss <neryssistos@gmail.com>
2021-06-24 15:51:58 +0200

    done

--- day01/ex03/main.cpp
+++ day01/ex03/main.cpp
@@ -2,25 +2,24 @@
 #include "HumanB.hpp"
 #include "Weapon.hpp"
 
-int	main(void)
+int main(void)
 {
 	{
-		Weapon	club = Weapon("crude spiked club");
-		HumanA	bob("bob", club);
+		Weapon club = Weapon("crude spiked club");
+		
+		HumanA bob("bob", club);
 		bob.attack();
 		club.setType("magnificient unbreakable diamon staff");
 		bob.attack();
-		std::cout << "Current weapon : " << club.getType() << std::endl;
 	}
 	{
-		Weapon	club = Weapon("crude spiked club");
+		Weapon club = Weapon("crude spiked club");
 
-		HumanB	jim("jim");
+		HumanB jim("jim");
 		jim.setWeapon(club);
 		jim.attack();
 		club.setType("wonderful eridium sword");
 		jim.attack();
-		std::cout << "Current weapon : " << club.getType() << std::endl;
 	}
 	return (0);
 }
