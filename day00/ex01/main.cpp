8e70ccbcf7c5502dc764978352e3425fee17839f
neryss <neryssistos@gmail.com>
2021-06-25 14:28:52 +0200

    patch

--- day00/ex01/main.cpp
+++ day00/ex01/main.cpp
@@ -1,58 +1,70 @@
 #include "Contact.hpp"
 
-int	add_contact(contact *book, int i)
+int	max_cont = 0;
+
+int	add_contact(contact *book, int &index)
 {
 	std::string	input;
 	std::string	tmp;
-	if (i >= 8)
-		return (1);
-	book[i].id = i;
-	std::cout << "Enter first name" << std::endl;
-	std::getline(std::cin, book[i].f_name);
-	if ((int)book[i].f_name.length() > 10)
+	int			ret;
+
+	ret = 0;
+	if (index >= 8)
 	{
-		tmp = book[i].f_name.substr(0, 10);
+		ret = 1;
+		index = 0;
+	}
+	book[index].id = index;
+	std::cout << "Enter first name" << std::endl;
+	std::cin.clear();
+	std::cin.ignore();
+	std::getline(std::cin, book[index].f_name);
+	if ((int)book[index].f_name.length() > 10)
+	{
+		tmp = book[index].f_name.substr(0, 10);
 		tmp[9] = '.';
 	}
 	std::cout << "Enter last name" << std::endl;
-	std::getline(std::cin, book[i].l_name);
-	if ((int)book[i].l_name.length() > 10)
+	std::getline(std::cin, book[index].l_name);
+	if ((int)book[index].l_name.length() > 10)
 	{
-		tmp = book[i].l_name.substr(0, 10);
+		tmp = book[index].l_name.substr(0, 10);
 		tmp[9] = '.';
 	}
 	std::cout << "Enter nickname" << std::endl;
-	std::getline(std::cin, book[i].n_name);
-	if ((int)book[i].n_name.length() > 10)
+	std::getline(std::cin, book[index].n_name);
+	if ((int)book[index].n_name.length() > 10)
 	{
-		tmp = book[i].n_name.substr(0, 10);
+		tmp = book[index].n_name.substr(0, 10);
 		tmp[9] = '.';
 	}
 	std::cout << "Enter login" << std::endl;
-	std::getline(std::cin, book[i].login);
+	std::getline(std::cin, book[index].login);
 	std::cout << "Enter postal code" << std::endl;
-	std::getline(std::cin, book[i].postal);
+	std::getline(std::cin, book[index].postal);
 	std::cout << "Enter email" << std::endl;
-	std::getline(std::cin, book[i].mail);
+	std::getline(std::cin, book[index].mail);
 	std::cout << "Enter phone number" << std::endl;
-	std::getline(std::cin, book[i].phone);
+	std::getline(std::cin, book[index].phone);
 	std::cout << "Enter birth date" << std::endl;
-	std::getline(std::cin, book[i].bday);
+	std::getline(std::cin, book[index].bday);
 	std::cout << "Enter favorite meal" << std::endl;
-	std::getline(std::cin, book[i].meal);
+	std::getline(std::cin, book[index].meal);
 	std::cout << "Enter your underwear color" << std::endl;
-	std::getline(std::cin, book[i].underwear);
+	std::getline(std::cin, book[index].underwear);
 	std::cout << "Enter your darkest secret" << std::endl;
-	std::getline(std::cin, book[i].secret);
-	std::cout << "Contact registered as nb: " << book[i].id << std::endl;
-	return (0);
+	std::getline(std::cin, book[index].secret);
+	std::cout << "Contact registered as nb: " << book[index].id << std::endl;
+	std::cin.clear();
+	index++;
+	return (ret);
 }
 
 void	display_book(contact *book, int i)
 {
 	int			index = 0;
 	std::string	tmp;
-	for (int j = 0; j < i; j++)
+	for (int j = 0; j < max_cont; j++)
 	{
 		std::cout.width(10);
 		std::cout << std::right << book[j].id;
@@ -91,9 +103,17 @@ void	display_book(contact *book, int i)
 	{
 		std::cout << "Chose the desired contact index" << std::endl;
 		std::cin >> index;
-		if (index < 0 || index >= i)
+		if (std::cin.fail())
+		{
+			std::cout << "Error: Cin failed" << std::endl;
+			std::cin.clear();
+			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+			return ;
+		}
+		else if (index < 0 || index >= i)
 		{
 			std::cout << "Error: Invalid index" << std::endl;
+			std::cin.clear();
 			return ;
 		}
 		std::cout << book[index].f_name << std::endl;
@@ -121,18 +141,19 @@ int	main(void)
 	i = 0;
 	while (!exit)
 	{
-		std::getline(std::cin, input);
+		std::cin.clear();
+		std::cin >> input;
 		if (input == "EXIT")
 			exit = true;
 		else if (input == "ADD")
 		{
-			if (!add_contact(book, i))
-				i++;
-			else
-				std::cout << "Max number of contact reached" << std::endl;
+			add_contact(book, i);
+			if (max_cont < 8)
+				max_cont++;
 		}
 		else if (input == "SEARCH")
 			display_book(book, i);
+		input = "";
 	}
 	return (0);
 }
