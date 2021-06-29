059e9efb1afa9f177fd40af78a709a050f849f63
neryss <neryssistos@gmail.com>
2021-06-29 15:09:56 +0200

    karen seems ok

--- day01/ex05/Karen.cpp
+++ day01/ex05/Karen.cpp
@@ -1,26 +1,46 @@
 #include "Karen.hpp"
 
-void	debug(void)
+void	Karen::debug(void)
 {
 	std::cout << "I love to get extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I just love it!" << std::endl;
 }
 
-void	info(void)
+void	Karen::info(void)
 {
-	std::cout << " cannot believe adding extrabacon cost more money. You don’t put enough! If you did I would not have to askfor it!" << std::endl;
+	std::cout << "I cannot believe adding extra bacon cost more money. You don’t put enough! If you did I would not have to askfor it!" << std::endl;
 }
 
-void	warning(void)
+void	Karen::warning(void)
 {
-	std::cout << " cannot believe adding extrabacon cost more money. You don’t put enough! If you did I would not have to askfor it!" << std::endl;
+	std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming here for years and you just started working here last month." << std::endl;
 }
 
-void	error(void)
+void	Karen::error(void)
 {
 	std::cout << "This is unacceptable, I want to speak to the manager now." << std::endl;
 }
 
 void	Karen::complain(std::string level)
 {
-	// Might need an enum
+	void (Karen::*fns[])(void) = {
+		&Karen::debug,
+		&Karen::info,
+		&Karen::warning,
+		&Karen::error
+	};
+	std::string	type[4] = {
+		"DEBUG",
+		"INFO",
+		"WARNING",
+		"ERROR"
+	};
+	for (int i = 0; i < 4; i++)
+	{
+		if (level == type[i])
+		{
+			(this->*fns[i])();
+			return ;
+		}
+	}
+	std::cerr << "Error: function: " << level << " not found" << std::endl;
 }
--- day01/ex05/main.cpp
+++ day01/ex05/main.cpp
@@ -1,1 +1,10 @@
 #include "Karen.hpp"
+
+int	main(int argc, char **argv)
+{
+	Karen karen;
+	if (argc <= 1 || argc > 2)
+		return (1);
+	karen.complain(argv[1]);
+	return (0);
+}
