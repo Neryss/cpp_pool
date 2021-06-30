f93d6c6bd7ed2b6cfc539bc1716fad4db99c405d
neryss <neryssistos@gmail.com>
2021-06-30 15:25:54 +0200

    wip fixed

--- day02/ex00/Fixed.hpp
+++ day02/ex00/Fixed.hpp
@@ -4,4 +4,20 @@
 #include <iostream>
 #include <string>
 
+class	Fixed
+{
+	private:
+		int					p_value;
+		const static int	p_bits = 8;
+	public:
+		Fixed()
+		:p_value(0)
+		{};
+		Fixed(const Fixed &f1)
+		:p_value(f1.p_value)
+		{};
+		~Fixed();
+
+};
+
 #endif
