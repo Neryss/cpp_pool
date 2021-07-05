cf07e5265dfdc46d16e22c8c9c92766efb85a417
neryss <neryssistos@gmail.com>
2021-07-05 21:54:41 +0200

    wip clapi

--- day03/ex00/ClapTrap.cpp
+++ day03/ex00/ClapTrap.cpp
@@ -1,6 +1,11 @@
 #include "ClapTrap.hpp"
 
+ClapTrap::ClapTrap()
+:_name("bob"), _hitpoints(10), _mana(10), _ad(0)
+{
+	std::cout << "ClapTrap created using the default constr" << std::endl;
+}
 void	ClapTrap::attack(std::string const &name)
 {
-
+	std::cout << _name << " attacks " << name << ", causing " << _ad << " points of damage!" << std::endl;
 }
