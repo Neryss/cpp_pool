129552a4c62f9480b84779639ba92aa6b0deb43f
neryss <neryssistos@gmail.com>
2021-07-13 15:32:57 +0200

    Better Makefile

--- day03/ex03/DiamondTrap.cpp
+++ day03/ex03/DiamondTrap.cpp
@@ -5,7 +5,7 @@ DiamondTrap::DiamondTrap() : ClapTrap()
 	std::cout << "Default DP constructor called\n" << std::endl;
 }
 
-DiamondTrap::DiamondTrap(std::string name) : ClapTrap(name + "_clap_name"), FragTrap(name + "_clap_name"), ScavTrap(name + "_clap_name"), _name(name)
+DiamondTrap::DiamondTrap(std::string name) : ClapTrap(name + "_clap_name"), FragTrap(name), ScavTrap(name), _name(name)
 {
 	std::cout << "<" + _name + "> Created!" << std::endl;
 }
