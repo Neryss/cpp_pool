885fa1c2c2bf971003dba147f741c25162560d06
neryss <neryssistos@gmail.com>
2021-07-14 16:45:12 +0200

    added tests

--- day03/ex01/main.cpp
+++ day03/ex01/main.cpp
@@ -5,6 +5,8 @@ int	main(void)
 {
 	ClapTrap	scavi("Clapi");
 	ScavTrap	enemy("bad-scavi");
+	ScavTrap	clone("clone");
+	ScavTrap	test(enemy);
 
 	scavi.attack("bad-scavi");
 	enemy.takeDamage(scavi.get_ad());
@@ -13,5 +15,8 @@ int	main(void)
 	enemy.takeDamage(scavi.get_ad());
 	enemy.beRepaired(enemy.get_ad() / 2);
 	enemy.guardGate();
+	test.guardGate();
+	test = clone;
+	test.guardGate();
 	return (0);
 }
