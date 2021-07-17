356ea245404f092cb4dfa711f56bf303a67504f5
neryss <neryssistos@gmail.com>
2021-07-17 15:37:41 +0200

    need to understand deep copies

--- day04/ex01/Brain.cpp
+++ day04/ex01/Brain.cpp
@@ -10,11 +10,14 @@ Brain::Brain()
 					"making some noise"};
 	for (int i = 0; i < 100; i++)
 		_ideas[i] = words[rand() % 100 + 1];
+	std::cout << "Brain created!" << std::endl;
 }
 
 Brain::Brain(const Brain &other)
 {
-
+	for (int i = 0; i < 100; i++)
+		_ideas[i] = other._ideas[i];
+	std::cout << "Brain created from another one!" << std::endl;
 }
 
 Brain::~Brain()
@@ -29,5 +32,7 @@ Brain	&Brain::operator=(const Brain &other)
 
 void	Brain::displayIdeas()
 {
-	
+	std::cout << "Here are all their ideas" << std::endl;
+	for (int i = 0; i < 100; i++)
+		std::cout << _ideas[i] << std::endl;
 }
