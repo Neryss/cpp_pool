76eeed77767e25af242d28741573a52a5391e228
neryss <neryssistos@gmail.com>
2021-07-28 11:17:19 +0200

    base done

--- day05/ex00/Bureaucrat.cpp
+++ day05/ex00/Bureaucrat.cpp
@@ -1,1 +1,23 @@
 #include "Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat(std::string name, int grade)
+:_name(name), _grade(grade)
+{
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat &other)
+:_name(other._name), _grade(other._grade)
+{
+}
+
+Bureaucrat	&Bureaucrat::operator=(const Bureaucrat &other)
+{
+	_name = other._name;
+	_grade = other._grade;
+	return (*this);
+}
+
+Bureaucrat::~Bureaucrat()
+{
+}
+
--- day05/ex00/Bureaucrat.hpp
+++ day05/ex00/Bureaucrat.hpp
@@ -8,7 +8,26 @@ class Bureaucrat
 {
 	private:
 		std::string	_name;
-
+		int			_grade; // [1-150]
+		Bureaucrat();
+	public:
+		Bureaucrat(std::string name, int grade);
+		Bureaucrat(const Bureaucrat &other);
+		~Bureaucrat();
+		std::string	getName() const;
+		int			getGrade() const;
+		void		incrementGrade();
+		void		decrenebtGrade();
+		Bureaucrat	&operator=(const Bureaucrat &other);
+		class GradeTooHighException : std::exception
+		{
+			const char	*what () const throw ()
+			{
+				return ("Grade too high exception!\n");
+			}
+		};
 };
+
+std::ostream	&operator<<(std::ostream &os, const Bureaucrat &b);
 
 #endif
