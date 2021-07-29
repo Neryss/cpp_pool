dd76106d0a58d209717ea0ca79ed4bacb8f4ee6c
neryss <neryssistos@gmail.com>
2021-07-29 18:38:27 +0200

    main should be ok

--- day05/ex02/main.cpp
+++ day05/ex02/main.cpp
@@ -7,25 +7,26 @@ int	main()
 {
 	try
 	{
-		ShrubberyCreationForm	s1("home");
-		RobotomyRequestForm		r1("yikes");
-		PresidentialPardonForm	p1("hello");
+		// Form	cant("test", 10, 10);
+		ShrubberyCreationForm	s1("home");	// 145 137
+		RobotomyRequestForm		r1("yikes"); // 72 45
+		PresidentialPardonForm	p1("hello"); // 25 5
 		PresidentialPardonForm	p2("yo");
+		Bureaucrat				sbire("sbire", 15);
 		p2 = p1;
 
 		std::cout << p1.getTarget() << " " << p2.getTarget() << std::endl;
 		Bureaucrat	bob("bob", 100);
 		bob.signForm(r1);
 		bob.executeForm(r1);
-		// Bureaucrat	noob("noob", 80);
-		// s1.beSigned(bob);
-		// s1.beSigned(noob);
-		// // noob.signForm(r1);
-		// s1.execute(bob);
-		// r1.execute(bob);
-		// bob.signForm(p1);
-		// p1.execute(bob);
-		// bob.executeForm(p1);
+		sbire.signForm(r1);
+		r1.execute(sbire);
+		Bureaucrat	noob("noob", 80);
+		// sbire.signForm(p2);	// can sign
+		// sbire.executeForm(p2); // can't execute
+		// both ok
+		s1.beSigned(noob);
+		s1.execute(bob);
 		return (0);
 	}
 	catch (std::exception &e)
