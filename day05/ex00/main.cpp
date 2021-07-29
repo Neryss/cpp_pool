42b82ef3472fd1c168d426d9f89a7591ffa53dd0
neryss <neryssistos@gmail.com>
2021-07-29 17:13:42 +0200

    main kinda ok

--- day05/ex00/main.cpp
+++ day05/ex00/main.cpp
@@ -4,23 +4,30 @@ int	main()
 {
 	try
 	{
+		std::cout << "Constructors and copy" << std::endl;
 		Bureaucrat	bob("bob", 150);
 		Bureaucrat	test(bob);
 		Bureaucrat	salut("test", 15);
-		salut =  Bureaucrat("lol", 10);
-		// salut->decrementGrade();
+		salut = Bureaucrat("lol", 10);
+		std::cout << "Incrementing and decrementing" << std::endl;
+		std::cout << salut;
+		salut.decrementGrade();
+		std::cout << salut;
+		std::cout << bob;
 		bob.incrementGrade();
+		std::cout << bob;
+		//	Following will throw exception because grade will be past 150
 		// for (int i = 0; i < 3; i++)
 		// {
 		// 	std::cout << bob;
 		// 	bob.decrementGrade();
 		// }
+		// Will throw exception because grade will be too high
 		// for (int i = 0; i < 10; i++)
 		// {
-		// 		std::cout << salut;
 		// 		salut.incrementGrade();
+		// 		std::cout << salut;
 		// }
-		std::cout << bob;
 		std::cout << salut;
 		std::cout << test;
 		return (0);
