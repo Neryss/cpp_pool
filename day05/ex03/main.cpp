896caaa3bfe5f2d87ff762ca25ae07aab17300da
neryss <neryssistos@gmail.com>
2021-07-29 19:45:31 +0200

    leaks

--- day05/ex03/main.cpp
+++ day05/ex03/main.cpp
@@ -10,12 +10,21 @@ int	main()
 	try
 	{
 		Bureaucrat	bob("bob", 1);
+		Bureaucrat	tet("tet", 40);
 		Intern		intern;
 		Form		*rrf;
 		std::string	targets[4] = {"presidential pardon", "robotomy request", "shrubbery creation", "blablabla"};
 
 		srand(time(NULL));
 		rrf = intern.makeForm(targets[rand() % 4], "idk");
+		if (!rrf)
+			return (1);
+		std::cout << *rrf;
+		tet.signForm(*rrf);
+		bob.executeForm(*rrf);
+		tet.executeForm(*rrf);
+		rrf->execute(tet);
+		std::cout << "ALED" << std::endl;
 		delete rrf;
 		return (0);
 	}
