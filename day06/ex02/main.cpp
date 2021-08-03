3eb6215a92c466669cd90b4a6a50e2708cf3971a
neryss <neryssistos@gmail.com>
2021-08-03 18:35:02 +0200

    generate done

--- day06/ex02/main.cpp
+++ day06/ex02/main.cpp
@@ -3,8 +3,36 @@
 #include "B.hpp"
 #include "C.hpp"
 #include <iostream>
+#include <time.h>
+
+Base	*generate(void)
+{
+	Base	*bas;
+	srand(time(NULL));
+	int random = rand() % 3;
+	std::cout << random << std::endl;
+	if (random % 3 == 0)
+		return (bas = new A);
+	else if (random % 3 == 1)
+		return (bas = new B);
+	else if (random % 3 == 2)
+		return (bas = new C);
+	return (nullptr);
+}
+
+void	identify(Base *p)
+{
+	
+}
+
+void	identify(Base &p)
+{
+
+}
 
 int main(void)
 {
-
+	Base	*rb = generate();
+	delete rb;
+	return (0);
 }
