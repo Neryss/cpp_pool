d6aff23f96221100f433d09e9aeb7b55eed31b83
neryss <neryssistos@gmail.com>
2021-08-03 17:33:09 +0200

    wip

--- day06/ex01/main.cpp
+++ day06/ex01/main.cpp
@@ -9,10 +9,19 @@ struct Data
 
 uintptr_t	serialize(Data *ptr)
 {
-	
+	return (reinterpret_cast<uintptr_t>(ptr));
+}
+
+Data	*deserialize(uintptr_t raw)
+{
+	return (reinterpret_cast<Data*>(raw));
 }
 
 int main(void)
 {
-	
+	Data	bob;
+	bob._name = "Bob";
+	bob._level = 10;
+	std::cout << bob._name << " lvl " << bob._level << std::endl;
+	return (0);
 }
