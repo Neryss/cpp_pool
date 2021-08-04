d63556deda0e9ff0d14154ff3c0b0d3eab23a3c2
neryss <neryssistos@gmail.com>
2021-08-04 04:01:57 +0200

    added try catch

--- day07/ex02/main.cpp
+++ day07/ex02/main.cpp
@@ -8,5 +8,14 @@ int main(void)
 		i1[i] = i;
 	for (int i = 0; i < 5; i++)
 		std::cout << i1[i] << std::endl;
+	try
+	{
+		std::cout << i1[10] << std::endl;
+	}
+	catch (const std::exception &e)
+	{
+		std::cerr << e.what() << std::endl;
+		return (1);
+	}
 	return (0);
 }
