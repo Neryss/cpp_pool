cdc40b4294975c42edf2d5b23ea9b87ff9a5bc9f
neryss <neryssistos@gmail.com>
2021-08-17 14:43:24 +0200

    copy not working

--- day08/ex01/main.cpp
+++ day08/ex01/main.cpp
@@ -5,20 +5,23 @@ int main(void)
 {
 	Span test(5);
 	Span error(1);
-
-	test.addNumber(5);
-	test.addNumber(3);
-	test.addNumber(17);
-	test.addNumber(9);
-	test.addNumber(11);
-
-	error.addNumber(69);
+	Span copy(4);
+	copy = test;
 
 	try
 	{
+		test.addNumber(5);
+		test.addNumber(3);
+		test.addNumber(17);
+		test.addNumber(9);
+		test.addNumber(11);
+
+		error.addNumber(69);
+		// error.addNumber(79);
 		std::cout << test.shortestSpan() << std::endl;
 		std::cout << test.longestSpan() << std::endl;
-		std::cout << error.longestSpan() << std::endl;
+		std::cout << copy.longestSpan() << std::endl;
+		// std::cout << error.longestSpan() << std::endl;
 	}
 	catch (const std::exception &e)
 	{
