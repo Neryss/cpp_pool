22a54d0111c313be880702907cd19b8c4905b09e
neryss <neryssistos@gmail.com>
2021-08-17 15:45:58 +0200

    need to check again

--- day08/ex02/mutantstack.hpp
+++ day08/ex02/mutantstack.hpp
@@ -1,6 +1,21 @@
 #ifndef MUTANTSTACK_HPP
 #define MUTANTSTACK_HPP
 
+#include <vector>
+#include <stack>
+#include <iterator>
 
+template <typename T>
+class Mutantstack : public std::stack<T>
+{
+	public:
+		typedef typename std::stack<T>::container_type::iterator iterator;
+		Mutantstack();
+		Mutantstack(const Mutantstack &other);
+		Mutantstack	&operator=(const Mutantstack &other);
+		~Mutantstack();
+		iterator begin();
+		iterator end();
+};
 
 #endif
